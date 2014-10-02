44a4e1c08c6df4955030057572c0cc5815b8c722
SleepyCoder300 <desmond_taylor@msn.com>
2014-10-02 17:58:40 +0100

    Added blank engine structure

---
+++ include/global.h
@@ -0,0 +1,25 @@
+#ifndef __GLOBAL_H_INCLUDED
+#define __GLOBAL_H_INCLUDED
+
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_physfs.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <physfs.h>
+
+#include "fwd/engine/config.h"
+#include "fwd/engine/font.h"
+#include "fwd/engine/graphics.h"
+#include "fwd/engine/input.h"
+#include "fwd/engine/sound.h"
+
+#include "engine/config.h"
+#include "engine/font.h"
+#include "engine/graphics.h"
+#include "engine/input.h"
+#include "engine/sound.h"
+
+#endif // __GLOBAL_H_INCLUDED
